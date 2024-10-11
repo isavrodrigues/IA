ad9a9b4243b8768d8d5dd502f11e3058c36d9469
Isabela Rodrigues <68721253+isavrodrigues@users.noreply.github.com>
2024-10-11 16:27:04 -0300

    Update grafico.py

--- aps1/aps1-codigo/grafico.py
+++ aps1/aps1-codigo/grafico.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the CSV file
 df = pd.read_csv('tabela.csv')
 
-# Convert 'Tempo de processamento em segundos' to numeric, replacing 'NaN' with 0
 df['Tempo de processamento em segundos'] = pd.to_numeric(df['Tempo de processamento em segundos'], errors='coerce').fillna(0)
 
-# Create a list of unique algorithms
 algorithms = df['Agoritimo'].unique()
 
-# Create the plot
 plt.figure(figsize=(12, 6))
 
 for algorithm in algorithms:
@@ -21,10 +17,8 @@ plt.xlabel('Objetivo')
 plt.ylabel('Tempo de processamento (segundos)')
 plt.title('Comparação de Desempenho dos Algoritmos de Busca')
 plt.legend()
-plt.yscale('log')  # Use logarithmic scale for y-axis due to large range of values
+plt.yscale('log') 
 plt.grid(True)
-
-# Save the plot as a PNG file
 plt.savefig('search_algorithms_comparison.png')
 plt.close()
 
